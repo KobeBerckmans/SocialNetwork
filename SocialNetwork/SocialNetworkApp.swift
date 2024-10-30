4e33afa1a25a8d3896a7921803bed699bdd6ae49
KobeBerckmans <kobe.berckmans@student.ehb.be>
2024-10-30 21:32:09 +0100

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Kobe Berckmans on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
